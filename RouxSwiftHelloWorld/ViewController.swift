e82830c43905fe49f1e9f2a5c936170c9ae428ed
H. Cole Wiley <cole@scandy.co>
2020-05-12 17:43:46 -0500

    RouxSwiftHelloWorld working minimally

--- RouxSwiftHelloWorld/ViewController.swift
+++ RouxSwiftHelloWorld/ViewController.swift
@@ -6,13 +6,42 @@
 //  Copyright © 2020 Scandy. All rights reserved.
 //
 
-import UIKit
+import GLKit
 
-class ViewController: UIViewController {
+class ViewController: GLKViewController {
+  var context: EAGLContext?
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    let status = ScandyCore.setLicense()
+    print("license status: ", status)
+    
+    startPreview()
+  }
+  
+  func requestCamera() -> Bool {
+    if ( ScandyCore.hasCameraPermission() )
+    {
+      print("user has granted permission to camera!")
+      return true
+    } else {
+      print("user has denied permission to camera")
+    }
+    return false
+  }
+
+  
+  func startPreview() {
+    if( requestCamera() ) {
+      /*
+       This is a little ugly, we could make this ScandyCoreScannerType into
+       a better Swift class, but it works for now.
+       */
+      var scanner_type: ScandyCoreScannerType = ScandyCoreScannerType(rawValue: 5);
+      ScandyCore.initializeScanner(scanner_type)
+      ScandyCore.startPreview()
+    }
   }
 
 
