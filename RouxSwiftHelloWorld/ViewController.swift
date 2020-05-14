ae1a1458338bf838a11e79f7e8deeed1e04fed45
April Polubiec <aprilpolubiec@gmail.com>
2020-05-14 16:04:36 -0500

    bare bones app that scan start scan, stop scan, save mesh, but with some crashes

--- RouxSwiftHelloWorld/ViewController.swift
+++ RouxSwiftHelloWorld/ViewController.swift
@@ -9,16 +9,64 @@
 import GLKit
 
 class ViewController: GLKViewController {
+    
+    //MARK: Properties
+    @IBOutlet weak var stopScanButton: UIButton!
+    @IBOutlet weak var startScanButton: UIButton!
+    @IBOutlet weak var saveMeshButton: UIButton!
+    @IBOutlet weak var startPreviewButton: UIButton!
+    
+    //MARK: Actions
+    @IBAction func startScanningPressed(_ sender: Any) {
+        print("start scanning pressed");
+        startScanning();
+    }
+    
+    @IBAction func stopScanningPressed(_ sender: Any) {
+        print("stop scanning pressed");
+        startScanButton.isHidden = true;
+        stopScanButton.isHidden = true;
+        stopScanning();
+        ScandyCore.generateMesh();
+    }
+    
+    @IBAction func saveMeshPressed(_ sender: Any) {
+        let date = Date();
+        let formatter = DateFormatter();
+        formatter.dateFormat = "yyyy-MM-dd_HH-mm-ss";
+        let id = formatter.string(from: date);
+        // NOTE: You can change this to: obj, ply, or stl
+        let filetype = "ply";
+        let filename = "rouxiosexample_\(id).\(filetype)";
+        let documentspath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0];
+        let documentsURL = URL(fileURLWithPath: documentspath);
+        let fileURL = documentsURL.appendingPathComponent(filename);
+        let filepath = fileURL.absoluteString;
+        print("saving file to \(filepath)");
+        let alertController = UIAlertController(title: "Mesh Saved", message:
+               "file saved to \(filepath)", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
 
-  override func viewDidLoad() {
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func startPreviewPressed(_ sender: Any) {
+        print("start preview pressed");
+//      ScandyCore.reset();
+//      ScandyCore.setLicense();
+        turnOnScanner();
+    }
+
+    
+    override func viewDidLoad() {
     super.viewDidLoad()
     // Set the ScandyCoreLicense.txt
     let status = ScandyCore.setLicense()
     print("license status: ", status)
-    
     // call our function to start ScandyCore
-    turnOnScanner()
-  }
+    turnOnScanner();
+   }
   
   func requestCamera() -> Bool {
     if ( ScandyCore.hasCameraPermission() )
@@ -33,6 +81,14 @@ class ViewController: GLKViewController {
 
   
   func turnOnScanner() {
+//    scanSizeLabel.isHidden = false;
+//    scanSizeSlider.isHidden = false;
+    startScanButton.isHidden = false;
+    stopScanButton.isHidden = true;
+    
+    startPreviewButton.isHidden = true;
+    saveMeshButton.isHidden = true;
+
     if( requestCamera() ) {
       /*
        This is a little ugly, we could make this ScandyCoreScannerType into
@@ -46,6 +102,29 @@ class ViewController: GLKViewController {
       ScandyCore.setVoxelSize(mm * 1e-3)
     }
   }
+    
+ func startScanning() {
+    //Render our buttons
+    stopScanButton.isHidden = false;
+    startScanButton.isHidden = true;
+    //scanSizeLabel.isHidden = true;
+    //scanSizeSlider.isHidden = true;
+    //Make sure the preview is running
+    //if(ScandyCore.isRunning()){
+    ScandyCore.startScanning();
+   // };
+  }
+    
+func stopScanning() {
+    stopScanButton.isHidden = true;
+    saveMeshButton.isHidden = false;
+    startPreviewButton.isHidden = false;
+
+    //Make sure the preview is running
+    //if(ScandyCore.isRunning()){
+    ScandyCore.stopScanning();
+   // };
+ }
 
 
 }
