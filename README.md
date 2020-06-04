b54a51e309d1c0cb1e557ab8f05c05df89bd8398
H. Cole Wiley <cole@scandy.co>
2020-06-04 18:02:12 -0500

    added appcenter post clone for getting depencies

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A sample app showing how to us Roux with a Swift app.
 
+[![Build status](https://build.appcenter.ms/v0.1/apps/d66c9a08-0ae6-40da-803f-589875e56574/branches/master/badge)](https://appcenter.ms)
+
 ## Setup
 
 ### ScandyCore.framework
---
+++ appcenter-post-clone.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# make sure we always run from the bash script path
+_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+cd $_dir
+
+test_dir=$_dir/tmp
+mkdir -p $test_dir
+
+# download the license and SDK
+roux_license_name="ScandyCoreLicense.txt"
+roux_license_path="$_dir/$repo_name/$roux_license_name"
+roux_tar_name=$(echo $S3_ROUX_TAR | awk -F "s3://roux-builds/" '{print $2}')
+roux_dir_name=$(echo $roux_tar_name | awk -F ".tar.gz" '{print $1'})
+roux_dir_path=$test_dir/$roux_dir_name
+framework_name="ScandyCore.framework"
+
+# _dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+repo_name=$(basename $(git rev-parse --show-toplevel))
+
+echo "get $S3_ROUX_LICENSE"
+aws s3 cp --quiet $S3_ROUX_LICENSE $roux_license_path
+echo "get $S3_ROUX_TAR"
+aws s3 cp --quiet $S3_ROUX_TAR $test_dir/$roux_tar_name
+
+pushd $test_dir
+tar -zxf $roux_tar_name
+popd
+
+# remove the framework if its already there
+framework_dst=$_dir/Frameworks/$framework_name
+
+if [ -e $framework_dst ]; then
+  rm -rf $framework_dst
+fi
+
+
+ln -s $roux_dir_path/$framework_name $framework_dst
+echo "Linked $framework_dst"
